35e09dfaaca1b3cdaf96b42b802b218b5542cd65
John <john@pop-os.attlocal.net>
2025-05-28 10:04:23 -0500

    cleaned up boilerplate code

--- main.ts
+++ main.ts
@@ -28,40 +28,6 @@ export default class DishRAGPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		if (!this.settings.local)
-		{
-		}
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-
 		this.addRibbonIcon('dice', 'Greet', async () => {
 			try {
 			const test = await ollama.embeddings({ model: 'nomic-embed-text', prompt: 'The sky is blue because of rayleigh scattering' });
@@ -73,27 +39,11 @@ export default class DishRAGPlugin extends Plugin {
 		});
 
 		this.addCommand({
-			id: 'spit_out_reply',
-			name: 'Query Local LLM (non-RAG)',
-			editorCallback: async (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				const prompt = editor.getSelection();
-
-				if (!prompt) {
-					new Notice("No prompt selected!");
-					return;
-				}
-
-				const test = await ollama.chat({ model: 'deepseek-r1:7b', messages: [{role: 'user', content: JSON.stringify(prompt)}] });
-				editor.replaceSelection(JSON.stringify(test.message.content));
-			}
-		});
-		this.addCommand({
 			id: 'spit_out_reply_simple_rule',
 			name: 'Query Local LLM (with simple rules)',
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				const prompt = editor.getSelection();
-				const ruleFilePath = "dishragrules.md";
+				const ruleFilePath = `${this.settings.rules_dir}dishragrules.md`;
 				const file = this.app.vault.getFileByPath(ruleFilePath);
 
 				new Notice('Successfully read rules file!');
@@ -112,12 +62,37 @@ export default class DishRAGPlugin extends Plugin {
 				const content = await this.app.vault.read(file);
 
 				const ruleGenContext = content;//await ollama.embeddings({model: 'nomic-embed-text', prompt: JSON.stringify(content)});
+				let response: any = null;
 
 				const gen_prompt = JSON.stringify(prompt).concat(JSON.stringify(ruleGenContext));
 
-				new Notice('Generating Response!');
-				const test = await ollama.chat({ model: 'deepseek-r1:7b', messages: [{role: 'user', content: gen_prompt}] });
-				editor.replaceSelection(test.message.content.replace(/<think>[\s\S]*?<\/think>/g, '').trim());
+				if (this.settings.local)
+				{
+					new Notice(`Generating Response with local LLM ${this.settings.model}!`);
+					response = await ollama.chat({ model: this.settings.model, messages: [{role: 'user', content: gen_prompt}] });
+					/*const result = await fetch("http://localhost:11434/api/chat", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						model: this.settings.model,
+						messages: [
+							{role: 'user',
+							content: gen_prompt} ]
+					}),
+					});
+
+					const response = await result.json();*/
+
+					editor.replaceSelection(response.message.content.replace(/<think>[\s\S]*?<\/think>/g, '').trim());
+
+				} else {
+					new Notice(`Generating Response with web LLM ${this.settings.model}!`);
+					const openai = new OpenAI({apiKey: this.settings.api_key})
+
+					const result = await openai.chat.completions.create({ model: this.settings.model, store: true, messages: [{"role": 'user', "content": gen_prompt}] });
+					response = result.choices[0].message
+					editor.replaceSelection(response);
+				}
 			}
 		});
 		this.addCommand({
@@ -125,8 +100,8 @@ export default class DishRAGPlugin extends Plugin {
 			name: 'Query Local LLM (with plaintext RAG)',
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				const prompt = editor.getSelection();
-				const ragFilePath = `${this.settings.rules_dir}dishragrules.md`;
-				const ruleFile = this.app.vault.getFileByPath(ragFilePath);
+				const ruleFilePath = `${this.settings.rules_dir}dishragrules.md`;
+				const ruleFile = this.app.vault.getFileByPath(ruleFilePath);
 
 				new Notice('Successfully read rules file!');
 
@@ -165,44 +140,35 @@ export default class DishRAGPlugin extends Plugin {
 				{
 					new Notice(`Generating Response with local LLM ${this.settings.model}!`);
 					response = await ollama.chat({ model: this.settings.model, messages: [{role: 'user', content: gen_prompt}] });
+					/*const result = await fetch("http://localhost:11434/api/chat", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						model: this.settings.model,
+						messages: [
+							{role: 'user',
+							content: gen_prompt} ]
+					}),
+					});
+
+					const response = await result.json();*/
+
+					editor.replaceSelection(response.message.content.replace(/<think>[\s\S]*?<\/think>/g, '').trim());
+
 				} else {
 					new Notice(`Generating Response with web LLM ${this.settings.model}!`);
 					const openai = new OpenAI({apiKey: this.settings.api_key})
 
 					const result = await openai.chat.completions.create({ model: this.settings.model, store: true, messages: [{"role": 'user', "content": gen_prompt}] });
 					response = result.choices[0].message
+					editor.replaceSelection(response);
 				}
 
-				if (response == null) {
-					new Notice('Error accessing the model!');
-					editor.replaceSelection('Error')
-
-				}
-				editor.replaceSelection(response.message.content.replace(/<think>[\s\S]*?<\/think>/g, '').trim());
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new DishRAGSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -227,23 +193,8 @@ export default class DishRAGPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class DishRAGSettingTab extends PluginSettingTab {
 	plugin: DishRAGPlugin;
 
 	constructor(app: App, plugin: DishRAGPlugin) {
